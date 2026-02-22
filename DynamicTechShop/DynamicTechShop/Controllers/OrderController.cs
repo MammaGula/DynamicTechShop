23549c262348ca3028c96b5949297417fa2a3ac1
MammaGula <supaphit.rue2@gmail.com>
2026-02-22 23:22:42 +0100

    - Improved small details

--- DynamicTechShop/DynamicTechShop/Controllers/OrderController.cs
+++ DynamicTechShop/DynamicTechShop/Controllers/OrderController.cs
@@ -1,6 +1,11 @@
-﻿using DynamicTechShop.Data;
+﻿using Azure.Core;
+using DynamicTechShop.Data;
 using DynamicTechShop.Shared.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using System.Collections;
+using static System.Net.WebRequestMethods;
 
 namespace DynamicTechShop.Controllers;
 
@@ -24,3 +29,18 @@ public class OrdersController : ControllerBase
         return Ok(order);
     }
 }
+
+
+//1. The user opens the website https://localhost:xxxx
+
+//2. The server(DynamicTechShop) sends the start page + static files.
+
+//3. The browser loads the client(WASM) and begins interactive interaction.
+
+//4. Products are read from products.json (static) → list/search is performed.
+
+//5. The cart is stored in LocalStorage(in the browser).
+
+//6. The checkout button is pressed → a POST request(/api/orders) is sent.
+
+//7. The server receives the request via the controller → EF Core saves it to the SQL Server(Orders table).
