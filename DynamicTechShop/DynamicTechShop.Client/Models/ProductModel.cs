0f4b5cbad196e99563de793e351e564d047c8771
MammaGula <supaphit.rue2@gmail.com>
2026-02-22 12:56:29 +0100

    Add project files.

---
+++ DynamicTechShop/DynamicTechShop.Client/Models/ProductModel.cs
@@ -0,0 +1,11 @@
+﻿namespace DynamicTechShop.Client.Models;
+
+public class ProductModel
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+    public string Category { get; set; } = "";
+    public string Description { get; set; } = "";
+    public string ImageUrl { get; set; } = "";
+    public decimal Price { get; set; }
+}
